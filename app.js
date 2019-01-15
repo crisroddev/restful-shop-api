679ff68aa129abe94ade5f1a8e340348c3bb99be
crodriguez85 <cristianrodriguez@Cristians-MacBook-Air.local>
2019-01-15 08:46:02 -0300

    Prodicut and orders routes

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
+const ordersRoutes = require('./api/routes/orders');
+const productRoutes = require('./api/routes/products');
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works'
-    });
-});
+app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
 
 module.exports = app;
