aa22f86d88a5c02ede1554e95579d445cf8b1e61
crodriguez85 <cristianrodriguez@Cristians-MacBook-Air.local>
2019-01-15 15:34:48 -0300

    CRUD operators for products

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const ordersRoutes = require('./api/routes/orders');
-const productRoutes = require('./api/routes/products');
-const morgan = require('morgan');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
-app.use(morgan('dev'));
+mongoose
+.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@node-rest-shop-iha84.mongodb.net/test?retryWrites=true`, {
+    useNewUrlParser: true
+});
 
-
-app.use('/products', productRoutes);
-app.use('/orders', ordersRoutes);
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(eror.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-     });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
 });
 
 module.exports = app;
