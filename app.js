e576c99ca0c4c895bf9fdf1743db40e6becf900e
crodriguez85 <cristianrodriguez@Cristians-MacBook-Air.local>
2019-01-15 08:59:03 -0300

    handling errors

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const ordersRoutes = require('./api/routes/orders');
 const productRoutes = require('./api/routes/products');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
 
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(eror.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+     });
+});
 
 module.exports = app;
