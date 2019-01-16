ebfb8e422a992221abc6b3e9649b1d300c676a3b
crodriguez85 <cristianrodriguez@Cristians-MacBook-Air.local>
2019-01-16 19:29:03 -0300

    User Login and delete

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,16 +4,64 @@ const mongoose = require("mongoose");
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
+router.post('/signup', (req, res, next) => {
+    User.find({
+            email: req.body.email
+        })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: 'Mail Exists'
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        })
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(result => {
+                                console.log(result);
+                                return res.status(201).json({
+                                    message: 'User Created'
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                })
+                            });
+                    }
+                });
 
-router.post('/signup', (req, resp, next) =>{
-    const user = new User ({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: bcrypt.hash(req.body.password)
-
-    });
+            }
+        })
 });
 
+router.delete('/:userId', (req, res, next) => {
+    User.remove({ _id: req.params.userId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'User Deleted'
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 
+});
 
 module.exports = router;
